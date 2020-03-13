1ce647072770b3ff1732ca8c58f4967c1f18759f
parrot <parrot@localhost.localdomain>
2020-03-13 11:34:58 -0500

    Update to ec2snapshot.py adding availability zone, status, instance type, and public dns

--- snapshot-script/ec2snapshot.py
+++ snapshot-script/ec2snapshot.py
@@ -4,9 +4,18 @@ import click
 session = boto3.Session(profile_name='snapshot-creator')
 ec2 = session.resource('ec2')
 
+@click.command()
 def list_instances():
+    "List EC2 instances"
     for i in ec2.instances.all():
-        print(i)
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
 
+    return
+    
 if __name__ == '__main__':
     list_instances()
