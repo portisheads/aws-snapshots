4c9c77da57e3aeafed455842477685bf1c1661ae
parrot <parrot@localhost.localdomain>
2020-03-14 12:40:44 -0500

    Added message 'snapshot already in progress' if a snapshot is currently being taken of the volume

--- snapshot-script/ec2snapshot.py
+++ snapshot-script/ec2snapshot.py
@@ -16,6 +16,10 @@ def filter_instances(project):
 
     return instances
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Snapshot-creator manages snapshots"""
@@ -88,14 +92,23 @@ def create_snapshots(project):
 
     for i in instances:
         print('Stopping {0}...'.format(i.id))
+
         i.stop()
         i.wait_until_stopped()
+
         for v in i.volumes.all():
+            if has_pending_snapshot(v):
+                print('Skipping {0}, snapshot already in progress'.format(v.id))
+                continue
+
             print('Creating snapshot of {0}'.format(v.id))
             v.create_snapshot(Description='Created by snapshot-creator')
+
         print('Starting {0}...'.format(i.id))
+
         i.start()
         i.wait_until_running()
+        
     print("Job's done!")
 
     return
